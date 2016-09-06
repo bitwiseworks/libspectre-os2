5d81a8d16fc4989199fe05484545b9296b5e7906
Silvan Scherrer <silvan.scherrer@aroa.ch>
2016-09-06 15:03:52 +0000

    libspectre: remove unneded changes, as fixed upstream
    git-svn-id: http://svn.netlabs.org/repos/ports/libspectre/trunk@1691 8d6eb4b1-0faa-de11-b929-00e081727c95

--- libspectre/spectre-gs.c
+++ libspectre/spectre-gs.c
@@ -55,12 +55,12 @@ critic_error_code (int code, char *function)
 	fprintf (stderr, "libspectre: error in %s found. Message see below.\n", function);
 	if (code <= -100) {
 		switch (code) {
-			case gs_error_Fatal:
+			case e_Fatal:
 				fprintf (stderr, "fatal internal error %d", code);
 				return TRUE;
 				break;
 
-			case gs_error_ExecStackUnderflow:
+			case e_ExecStackUnderflow:
 				fprintf (stderr, "stack overflow %d", code);
 				return TRUE;
 				break;
@@ -121,9 +121,9 @@ spectre_gs_process (SpectreGS  *gs,
 		set = _spectre_strdup_printf ("%d %d translate\n", -x, -y);
 		error = gsapi_run_string_continue (ghostscript_instance, set, strlen (set),
 						   0, &exit_code);
-		error = error == gs_error_NeedInput ? 0 : error;
+		error = error == e_NeedInput ? 0 : error;
 		free (set);
-		if (error != gs_error_NeedInput && critic_error_code (error, "spectre_gs_process 2")) {
+		if (error != e_NeedInput && critic_error_code (error, "spectre_gs_process 2")) {
 			fclose (fd);
 			return FALSE;
 		}
@@ -138,7 +138,7 @@ spectre_gs_process (SpectreGS  *gs,
 		read = fread (buf, sizeof (char), to_read, fd);
 		error = gsapi_run_string_continue (ghostscript_instance,
 						   buf, read, 0, &exit_code);
-		error = error == gs_error_NeedInput ? 0 : error;
+		error = error == e_NeedInput ? 0 : error;
 		left -= read;
 	}
 	
